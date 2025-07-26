d3e863b8c79ed525d44d11f4484d589db226ff14
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-07-26 09:01:00 -0300

    callbacks

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,4 +1,11 @@
-const {emailTemplate}= require('./js-foundation/01-template.js');
-require('./js-foundation/02-destructuring.js');
+//const {emailTemplate}= require('./js-foundation/01-template.js');
+//require('./js-foundation/02-destructuring.js');
+const {getUsesById} = require('./js-foundation/03-callbacks.js');
 
-//console.log(emailTemplate);
+const id=3;
+getUsesById(id, function(error, user){
+    if(error){
+        throw new Error(`Usuario no encontrado con el id: ${id}`);
+    }
+    console.log(user);
+})
