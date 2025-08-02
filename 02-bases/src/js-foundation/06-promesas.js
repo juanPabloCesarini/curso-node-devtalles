75c88aff30b34d5b3ac5ac354ce328cbfb881252
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-08-02 09:53:29 -0300

    uso de async await y axios

--- 02-bases/src/js-foundation/06-promesas.js
+++ 02-bases/src/js-foundation/06-promesas.js
@@ -1,12 +1,19 @@
-const getPokemonById = (id ) =>{
+const {http} = require('../plugins');
+const getPokemonById = async (id ) =>{
 
     const url =`https://pokeapi.co/api/v2/pokemon/${id}`;
+/* 
+    const res = await fetch(url);
+    const pokemon = await res.json(); */
 
-    return fetch(url)
+    const pokemon = await http.get(url);
+
+    return pokemon.name;
+    /* return fetch(url)
         .then((res) => res.json())
       //  .then( () => {throw new Error('pokemon no existe')})
         .then((pokemon) => pokemon.name);
-    
+     */
 }
 
 module.exports = getPokemonById;
--- 02-bases/src/plugins/index.js
+++ 02-bases/src/plugins/index.js
@@ -1,8 +1,9 @@
-const {getAge}=require('./get-age.plugin');
-const { idConstructor } = require('./get-uuid.plugin');
-
+const { getAge } = require("./get-age.plugin");
+const { idConstructor } = require("./get-uuid.plugin");
+const { http } = require("./http-client.plugin");
 
 module.exports = {
-    getAge,
-    idConstructor,
-}
+  getAge,
+  idConstructor,
+  http,
+};
